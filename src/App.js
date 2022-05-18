a133dd7a85278349ce594ae55d86407c51896e11
emonmorol <ekramulhasane69@gmail.com>
2022-05-18 22:41:14 +0600

    firebase initialized

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
--- src/Pages/Login/Social.js
+++ src/Pages/Login/Social.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import auth from "../../firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const Social = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [firebaseError, setFirebaseError] = useState("");
+
+  if (error) {
+    setFirebaseError(error.message);
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    console.log(user);
+  }
+
   return (
     <>
       <div class="divider">OR</div>
-      <button class="btn btn-block btn-outline">Continue With Google</button>
+      <button
+        onClick={() => signInWithGoogle()}
+        class="btn btn-block btn-outline"
+      >
+        Continue With Google
+      </button>
     </>
   );
 };
